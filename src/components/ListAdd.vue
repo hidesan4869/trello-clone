d45e3d3b065327d386fd3689656c890fc7070436
hidesan4869 <t1123.gogo@gmail.com>
2020-12-13 15:13:50 +0900

    addボタンにアクションを追加

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,11 +1,15 @@
 <template>
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classlist" @submit.prevent="addlist">
     <input v-model="title"
            type="text"
            class="text-input"
            placeholder="Add new list"
+           @focusin="startEditing"
+           @focusout="finishEditing"
     >
-    <button type="submit" class="add-button">
+    <button type="submit" 
+            class="add-button"
+            v-if="isEditing || titleExists">
       Add
     </button>
   </form>
@@ -16,12 +20,34 @@ export default {
   data: function(){
     return {
       title: "",
+      isEditing: false,
     }
   },
+   computed: {
+    classlist() {
+      const classlist = ['addlist']
+      if(this.isEditing) {
+        classlist.push('active')
+      }
+      if(this.titleExists) {
+        classlist.push('addable')
+      }
+      return classlist
+    },
+    titleExists() {
+      return this.title.length > 0
+    },
+  },
   methods: {
-    addList: function(){
-      this.$store.dispatch('addlist', {title: this.title})
+    addlist(){
+      this.$store.dispatch('addlist', this.title)
       this.title = ''
+    },
+    startEditing() {
+      this.isEditing = true
+    },
+    finishEditing() {
+      this.isEditing = false
     },
   },
 }
